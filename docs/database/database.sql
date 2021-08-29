00ab3b0acf2f23c2c41abef8f14aec8db72a62ec
sajjad <sajjad2251384@gmail.com>
2021-08-29 17:27:34 +0430

    1) create database products 2) create three table for app, releases and users

---
+++ docs/database/database.sql
@@ -0,0 +1,35 @@
+-- create database
+CREATE DATABASE IF NOT EXISTS products;
+
+
+-- use of database
+USE products;
+
+
+-- create tables
+
+CREATE TABLE IF NOT EXISTS applications(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS releases(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    app_id INT NOT NULL,
+    version VARCHAR(64) NOT NULL,
+    changelog TEXT NOT NULL,
+    date DATETIME,
+    FOREIGN KEY(app_id) REFERENCES applications(id)
+);
+
+CREATE TABLE IF NOT EXISTS users(
+    id VARCHAR(255) PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    upload BOOLEAN NOT NULL DEFAULT false,
+    token VARCHAR(255)
+);
