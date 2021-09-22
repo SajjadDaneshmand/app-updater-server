bc55b74df9bd2dd6bcd9ea4154f6edbbf5497efa
sajjad <sajjad2251384@gmail.com>
2021-09-22 09:59:45 +0330

    add site path to settings file

--- src/settings.py
+++ src/settings.py
@@ -7,6 +7,8 @@ BASE_DIR = os.path.dirname(
     )
 )
 
+UPLOAD_FOLDER = os.path.join(BASE_DIR, 'uploads')
+SITE_PATH = 'http://127.0.0.1:5000'
 INFO_FILE_NAME = 'info.conf'
 INFO_FiLE_PATH = os.path.join(BASE_DIR, INFO_FILE_NAME)
 
